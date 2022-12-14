10f3ffa908efba0d64c1048d482eb3bfef3df8e0
bbnguyenbb <bbnguyenbb@live.com>
2022-12-14 15:05:08 +0700

    hoàn tất

---
+++ CSharp/ForeachWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Text;
+
+Console.InputEncoding  = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+string[] arrs = {"nguyên","hùng","phú","khánh", "thảo","lợi","vinh"};
+
+foreach (var t in arrs)
+{
+    Console.WriteLine($"{t} có chiều dài {t.Length}");
+}
+
+//int i = 0;
+//while (i < arrs.Length)
+//{
+//    Console.WriteLine($"{arrs[i]} có chiều dài {arrs[i].Length}");
+//    i++;
+//}
+
+IEnumerator e = arrs.GetEnumerator();
+while (e.MoveNext())
+{
+    string s = (string)e.Current;
+    Console.WriteLine($"{s} có chiều dài {s.Length}");
+}
--- CSharp/QuestioMark/Program.cs
+++ CSharp/QuestioMark/Program.cs
@@ -7,3 +7,27 @@ Console.WriteLine(a + b == 0.3 ? "true" : "false");
 decimal c = 0.1M;
 decimal d = 0.2M;
 Console.WriteLine(c + d == 0.3M ? "true" : "false");
+
+// ?: => ternary operator
+
+// ? nullable value type
+
+string? fullName = null;
+//int i = null; => error vì kiều số ko thể null
+//c# cũ
+//Nullable<int> i=null;
+//c# mới
+bool? check = null;
+
+//int length = fullName != null ? fullName.Length : 0;
+
+//int? length = fullName is not null ? fullName.Length : null;
+
+
+//null-conditional operator  ?
+//tương tự != , fullname mà khác null thì trả về vế sau.
+int? lenght = fullName?.Length;
+
+//null-coalescing operator ??
+//tương tự như dấu ==, fullname bằng null thì trả về vế sau
+int? chieudai = fullName?.Length ?? 0;
